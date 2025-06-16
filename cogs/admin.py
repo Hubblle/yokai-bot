fe8ab2d3f62675d980615b4c33711b52e3b6cfc6
hubblle <lubin.tsch@gmail.com>
2025-06-16 20:30:06 +0200

    Remove the sync command

--- cogs/admin.py
+++ cogs/admin.py
@@ -424,42 +424,6 @@ class Admin_command(commands.Cog):
             return await ctx.send(embed=sucess_embed)
                 
                 
-                
-                
-    @commands.hybrid_command(
-        name="sync",
-        description="Synchonizes the slash commands.",
-    )
-    @commands.is_owner()
-    async def sync(self, context: commands.Context, scope: str) -> None:
-        """
-        Synchonizes the slash commands.
-
-        scope : The scope of the sync. Can be `global` or `guild`.
-        """
-
-        if scope == "global":
-            await context.bot.tree.sync()
-            embed = discord.Embed(
-                description="Slash commands have been globally synchronized.",
-                color=0xBEBEFE,
-            )
-            await context.send(embed=embed)
-            return
-        elif scope == "guild":
-            context.bot.tree.copy_global_to(guild=context.guild)
-            await context.bot.tree.sync(guild=context.guild)
-            embed = discord.Embed(
-                description="Slash commands have been synchronized in this guild.",
-                color=0xBEBEFE,
-            )
-            await context.send(embed=embed)
-            return
-        embed = discord.Embed(
-            description="The scope must be `global` or `guild`.", color=0xE02B2B
-        )
-        await context.send(embed=embed)
-
 
 async def setup(bot : commands.Bot ) -> None:
     await bot.add_cog(Admin_command(bot))
