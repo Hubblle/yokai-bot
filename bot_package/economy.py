9a3df6a22b6dab205a973b701744edb6f3a0784c
MrPheonixi <flechetarthur@icloud.com>
2026-01-01 16:04:47 +0100

    Refactor economy.py for user data management

--- bot_package/economy.py
+++ bot_package/economy.py
@@ -4,7 +4,8 @@ import discord
 from discord.ext import commands
 import bot_package.data as data
 
-
+# this function is call for saw if the user have data
+# and if he don't have info calculate he number of orbe
 async def create_user_info(user_id: int) -> None:
     if not str(user_id) in data.MONEY_DATA.keys():
         data.MONEY_DATA[str(user_id)] = 0
@@ -27,19 +28,20 @@ async def create_user_info(user_id: int) -> None:
             json.dump(data.MONEY_DATA, money_file, indent=4)
 
 
-
+# used to add orbe to a specific user
 async def add(user_id: int, amount: int) -> None:
     await create_user_info(user_id)
     data.MONEY_DATA[str(user_id)] += amount
     with open("./files/monnaie.json", "w") as money_file:
         json.dump(data.MONEY_DATA, money_file, indent=4)
 
-    
+# use to set at 0 the wallet of a spécific user
 async def reset(user_id: int) -> None:
     data.MONEY_DATA[str(user_id)] = 0
     with open("./files/monnaie.json", "w") as money_file:
         json.dump(data.MONEY_DATA, money_file, indent=4)
 
+# del the wallet of a specific user
 async def del_info(user_id: int) -> None:
     if str(user_id) in data.MONEY_DATA.keys():
         del data.MONEY_DATA[str(user_id)]
@@ -54,4 +56,5 @@ async def add_rank_orbe(user_id: int, rank) -> None:
     p = data.class_to_point[rank]
     data.MONEY_DATA[str(user_id)] += p
     with open("./files/monnaie.json", "w") as money_file:
+
         json.dump(data.MONEY_DATA, money_file, indent=4) 
