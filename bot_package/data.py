c56e8dd08edb4af05c7c9e759208f37fea6de168
Hubble <lubin.tsch@gmail.com>
2026-04-29 16:59:25 +0200

    Add comment

--- bot_package/data.py
+++ bot_package/data.py
@@ -152,7 +152,7 @@ blacklist = open_json("./files/blacklisted-yokai.json")
 default_medaillum  = {
                         "last_claim" : 10000,
                         "streak": [
-                            0,
+                            0, # Magic value, is never used or modified
                             "E",
                             0
                         ],
