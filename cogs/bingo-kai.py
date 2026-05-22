e90284d54e2c2b05a944ee1d2df1958281806638
Hubble <lubin.tsch@gmail.com>
2026-05-22 16:33:57 +0200

    Disable the terror event

--- cogs/bingo-kai.py
+++ cogs/bingo-kai.py
@@ -717,7 +717,7 @@ class Bingo_kai(commands.Cog):
                 yokai_embed.set_footer(text=message)
                 
                 
-            await ctx.send(embed=yokai_embed)
+            """await ctx.send(embed=yokai_embed)
             if equipped_treasure == "Trésor oni":
                     chance = data.item[equipped_treasure].get("value1")
             else :
@@ -725,7 +725,7 @@ class Bingo_kai(commands.Cog):
             if random.choices([True, False], weights=[chance, 100-chance])[0] :
                 evenement = event.Terrheure(self.bot)
                 await evenement.terrheure(ctx)
-   
+            """
    
 
 
